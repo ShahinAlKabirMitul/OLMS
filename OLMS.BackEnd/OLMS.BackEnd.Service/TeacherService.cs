be9f26a759ffd28cd88c00cf118e28d429c6312d
mitulbd <mitul.cse@gmail.com>
2017-10-26 00:12:45 +0600

    Add TeacherQuery Controller

--- OLMS.BackEnd/OLMS.BackEnd.Service/TeacherService.cs
+++ OLMS.BackEnd/OLMS.BackEnd.Service/TeacherService.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OLMS.BackEnd.Model;
 using OLMS.BackEnd.Repository;
+using OLMS.BackEnd.RequestModel;
+using OLMS.BackEnd.ViewModel;
 
 namespace OLMS.BackEnd.Service
 {
@@ -19,5 +22,43 @@ namespace OLMS.BackEnd.Service
         {
            return _repository.Add(teacher);
         }
+
+       public List<TeacherGridViewModel> Search(TeacherRequestModel request)
+       {
+           IQueryable<Teacher> teachers = this._repository.Get();
+
+           if (!string.IsNullOrWhiteSpace(request.Name))
+           {
+               teachers = teachers.Where(x => x.Name.ToLower().Contains(request.Name.ToLower()));
+           }
+           teachers = teachers.OrderBy(x => x.Modified);
+
+           if (!string.IsNullOrWhiteSpace(request.OrderBy))
+           {
+               if (request.OrderBy.ToLower() == "name")
+               {
+                   teachers = request.IsAscending ? teachers.OrderBy(x => x.Name) : teachers.OrderByDescending(x => x.Name);
+               }
+           }
+
+           teachers = teachers.Skip((request.Page - 1) * request.PerPageCount).Take(request.PerPageCount);
+
+           List<TeacherGridViewModel> list = teachers.ToList().ConvertAll(
+               teacher => new TeacherGridViewModel(teacher)
+           );
+
+           return list;
+       }
+
+       public TeacherDetailViewModel Detail(string id)
+       {
+           var teacher = _repository.GetDetail(id);
+           if (teacher == null)
+           {
+               throw new ObjectNotFoundException();
+           }
+           return new TeacherDetailViewModel(teacher);
+
+       }
     }
 }
