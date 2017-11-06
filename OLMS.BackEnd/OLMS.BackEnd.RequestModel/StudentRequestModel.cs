05c5e2386fc79390c2c3b10e48ec85d35ef06e2d
mitulbd <mitul.cse@gmail.com>
2017-11-06 20:54:45 +0600

    Add Extention Method Helper

--- OLMS.BackEnd/OLMS.BackEnd.RequestModel/StudentRequestModel.cs
+++ OLMS.BackEnd/OLMS.BackEnd.RequestModel/StudentRequestModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using OLMS.BackEnd.Model;
 
 namespace OLMS.BackEnd.RequestModel
 {
@@ -13,6 +15,21 @@ namespace OLMS.BackEnd.RequestModel
            
         }
 
+        private Expression<Func<Student, bool>> expression;
+        Expression<Func<Student, bool>> GetExpression()
+        {
+
+            if (!string.IsNullOrWhiteSpace(Name))
+            {
+                expression = x => x.Name.ToLower().Contains(Name.ToLower());
+            }
+            if (!string.IsNullOrWhiteSpace(Phone))
+            {
+                expression = expression.And(x => x.Phone.ToLower().Contains(Phone.ToLower()));
+            }
+            
+            return expression;
+        }
         public string Name { get; set; }
 
         public string Phone { get; set; }
