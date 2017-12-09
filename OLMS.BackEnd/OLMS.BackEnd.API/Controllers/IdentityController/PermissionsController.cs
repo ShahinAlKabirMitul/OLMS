0d2cc40f33cf60eef65df16f6c483db854ade96c
ShahinAlKabirMitul <mitul.cse@gmail.com>
2017-12-09 15:55:34 +0600

    Add controller

---
+++ OLMS.BackEnd/OLMS.BackEnd.API/Controllers/IdentityController/PermissionsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using OLMS.BackEnd.API.Models;
+
+namespace OLMS.BackEnd.API.Controllers.IdentityController
+{
+    public class PermissionsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Permissions
+        public IQueryable<Permission> GetPermissions()
+        {
+            return db.Permissions;
+        }
+
+        // GET: api/Permissions/5
+        [ResponseType(typeof(Permission))]
+        public async Task<IHttpActionResult> GetPermission(string id)
+        {
+            Permission Permission = await db.Permissions.FindAsync(id);
+            if (Permission == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Permission);
+        }
+
+        // PUT: api/Permissions/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPermission(string id, Permission Permission)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != Permission.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(Permission).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PermissionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Permissions
+        [ResponseType(typeof(Permission))]
+        public async Task<IHttpActionResult> PostPermission(Permission Permission)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Permissions.Add(Permission);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PermissionExists(Permission.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = Permission.Id }, Permission);
+        }
+
+        // DELETE: api/Permissions/5
+        [ResponseType(typeof(Permission))]
+        public async Task<IHttpActionResult> DeletePermission(string id)
+        {
+            Permission Permission = await db.Permissions.FindAsync(id);
+            if (Permission == null)
+            {
+                return NotFound();
+            }
+
+            db.Permissions.Remove(Permission);
+            await db.SaveChangesAsync();
+
+            return Ok(Permission);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PermissionExists(string id)
+        {
+            return db.Permissions.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ OLMS.BackEnd/OLMS.BackEnd.API/Controllers/IdentityController/ResourcesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using OLMS.BackEnd.API.Models;
+
+namespace OLMS.BackEnd.API.Controllers.IdentityController
+{
+    public class ResourcesController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Resources
+        public IQueryable<Resource> GetResources()
+        {
+            return db.Resources;
+        }
+
+        // GET: api/Resources/5
+        [ResponseType(typeof(Resource))]
+        public async Task<IHttpActionResult> GetResource(string id)
+        {
+            Resource resource = await db.Resources.FindAsync(id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(resource);
+        }
+
+        // PUT: api/Resources/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutResource(string id, Resource resource)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != resource.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(resource).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ResourceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Resources
+        [ResponseType(typeof(Resource))]
+        public async Task<IHttpActionResult> PostResource(Resource resource)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Resources.Add(resource);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ResourceExists(resource.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = resource.Id }, resource);
+        }
+
+        // DELETE: api/Resources/5
+        [ResponseType(typeof(Resource))]
+        public async Task<IHttpActionResult> DeleteResource(string id)
+        {
+            Resource resource = await db.Resources.FindAsync(id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            db.Resources.Remove(resource);
+            await db.SaveChangesAsync();
+
+            return Ok(resource);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ResourceExists(string id)
+        {
+            return db.Resources.Count(e => e.Id == id) > 0;
+        }
+    }
+}
