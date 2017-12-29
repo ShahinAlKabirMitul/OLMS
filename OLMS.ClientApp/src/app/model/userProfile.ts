eb8356d931b013dcb581e38a3d4c9a3ff3638b97
ShahinAlKabirMitul <mitul.cse@gmail.com>
2017-12-29 17:22:01 +0600

    change navbar

--- OLMS.ClientApp/src/app/model/userProfile.ts
+++ OLMS.ClientApp/src/app/model/userProfile.ts
@@ -9,25 +9,14 @@ export class UserProfile {
         currentUser: { id: '', userName: '', email: '',landingRoute:'' },
         claims: null
     };
-    constructor(private router: Router) {
-        
-    }
+    constructor(private router: Router) { }
 
     setProfile(profile: IProfile): void {
-console.log('set profile',profile);
 
-        
-      //  var userName = profile.
-      
-        //var userName = profile.claims.filter(p => p.type == 'userName')[0].value;
-     //   console.log('set email',userName);
         sessionStorage.setItem('access_token', profile['access_token']);
         sessionStorage.setItem('expires_in', profile['expires_in']);
-       // sessionStorage.setItem('nameid', nameid);
         sessionStorage.setItem('userName', profile['userName']);
         sessionStorage.setItem('landingRoute', profile['landingRoute']);
-       
-      
         this.userProfile.currentUser.id = sessionStorage.getItem('access_token');
         console.log(this.userProfile.currentUser.id);
     }
